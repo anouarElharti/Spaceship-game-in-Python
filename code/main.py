1940881856d70169185a8c5df5b73db89dd9c2f8
Anouar EL HARTI <B517AS@login.axa>
2024-10-01 12:12:04 +0200

    Making the plaine bounce arround like a DVD logo, adding delta time to capture frame / sec and match the speed of mouvement

--- code/main.py
+++ code/main.py
@@ -4,35 +4,59 @@ from random import randint
 
 # general setup
 pygame.init()
-WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 680
+WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 650
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 
 running = True
-
-# create a plain surface
-surf = pygame.Surface((100, 200))
-x, y = 100, 150
-surf.fill("red")
+clock = pygame.time.Clock()
 
 # importing image
 player_surface = pygame.image.load(join("images", "player.png")).convert_alpha()
+player_rec = player_surface.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+player_direction = pygame.math.Vector2(1, 1)
+player_speed = 200  # adjust this value to change the player speed
+
+# importing the stars
 stars_surface = pygame.image.load(join("images", "star.png")).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
-while True:
+# importing meteor
+meteor_surface = pygame.image.load(join("images", "meteor.png")).convert_alpha()
+meteor_rec = meteor_surface.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
+# importing the laser
+laser_surface = pygame.image.load(join("images", "laser.png")).convert_alpha()
+laser_rect = laser_surface.get_rect(bottomleft = (20, WINDOW_HEIGHT - 20))
+
+while running:
+    delta_time = clock.tick(10) / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # moving the player
+    player_rec.center += player_direction * player_speed * delta_time
+    if player_rec.bottom > WINDOW_HEIGHT or player_rec.top < 0:
+        player_direction.y *= -1
+    if player_rec.right > WINDOW_WIDTH or player_rec.left < 0:
+        player_direction.x *= -1
     # drawing the game
     display_surface.fill("darkgray")
-    # positioning the stars between the background surface and the player surface
+    # drawing the stars
     for position in star_positions:
         display_surface.blit(stars_surface, position)
-    # add the surf to the display
-    # x += .1 # animating the surface by adding to the x value
-    display_surface.blit(player_surface, (x, y))
+    # drawing the meteor
+    display_surface.blit(meteor_surface, meteor_rec)
+    # drawing the laser
+    display_surface.blit(laser_surface, laser_rect)
+
+    # drawing the player
+    display_surface.blit(player_surface, player_rec)
+
+    # updating the display
     pygame.display.update()
+
 
 pygame.quit()
