9be4a24ef617599c0ce1fa1bfe00df1d296cf061
Anouar EL HARTI <B517AS@login.axa>
2024-10-03 09:17:32 +0200

    adding movements to the player using key inputs

--- code/main.py
+++ code/main.py
@@ -14,8 +14,8 @@ clock = pygame.time.Clock()
 # importing image
 player_surface = pygame.image.load(join("images", "player.png")).convert_alpha()
 player_rec = player_surface.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1, 1)
-player_speed = 200  # adjust this value to change the player speed
+player_direction = pygame.math.Vector2()
+player_speed = 300  # adjust this value to change the player speed
 
 # importing the stars
 stars_surface = pygame.image.load(join("images", "star.png")).convert_alpha()
@@ -30,18 +30,43 @@ laser_surface = pygame.image.load(join("images", "laser.png")).convert_alpha()
 laser_rect = laser_surface.get_rect(bottomleft = (20, WINDOW_HEIGHT - 20))
 
 while running:
-    delta_time = clock.tick(10) / 1000
+    delta_time = clock.tick() / 1000
     # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # moving the player using inputs
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+        #     laser_rect.center = player_rec.center
+        # if event.type == pygame.MOUSEMOTION:
+        #     player_rec.center = event.pos
 
-    # moving the player
+    # inputs for mooving the player
+    keys = pygame.key.get_pressed()
+    # moving the player using inputs
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+
+    #maintaining the speed in every direction
+    if player_direction:
+        player_direction = player_direction.normalize()
+    else:
+        player_direction = player_direction
+
+    # capturing the fire strike
+    recent_keys = pygame.key.get_just_pressed()
+    if recent_keys[pygame.K_SPACE]:
+        print("Strike")
+    # applying the player direction
     player_rec.center += player_direction * player_speed * delta_time
-    if player_rec.bottom > WINDOW_HEIGHT or player_rec.top < 0:
-        player_direction.y *= -1
-    if player_rec.right > WINDOW_WIDTH or player_rec.left < 0:
-        player_direction.x *= -1
+    print((player_direction * player_speed).magnitude())
+
+    # moving the player like DVD logo
+    # player_rec.center += player_direction * player_speed * delta_time
+    # if player_rec.bottom >= WINDOW_HEIGHT or player_rec.top <= 0:
+    #     player_direction.y *= -1
+    # if player_rec.right >= WINDOW_WIDTH or player_rec.left <= 0:
+    #     player_direction.x *= -1
     # drawing the game
     display_surface.fill("darkgray")
     # drawing the stars
